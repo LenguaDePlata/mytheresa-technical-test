c3548a71a892e073508b15859d6fe78146fd22c5
Iñaki Iglesias <inaki@nukstudio.com>
2019-02-23 16:55:54 +0100

    Added models and relationships matching the migration; added route endpoints and stub controllers

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CartController extends Controller
+{
+	public function show(Cart $cart)
+	{
+
+	}
+
+	public function store(Item $item)
+	{
+
+	}
+
+	public function update(Cart $cart, Item $item)
+	{
+		
+	}
+}
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,6 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('carts/{id}', 'CartController@show');
+Route::put('carts/item/{item}', 'CartController@store');
+Route::put('carts/{cart}/item/{item}', 'CartController@update');
