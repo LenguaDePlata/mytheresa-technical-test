9a014b8131541b57586bd7c54f83031759cc62b6
Iñaki Iglesias <inaki@nukstudio.com>
2019-02-23 15:51:06 +0100

    Added database migrations

---
+++ database/migrations/2019_02_23_142814_create_carts_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('cart_id');
+            $table->unsignedInteger('item_id');
+            $table->unsignedInteger('quantity')->default(1);
+            $table->timestamps();
+
+            $table->unique(['cart_id', 'index_id']);
+            $table->foreign('cart_id')->references('id')->on('carts');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts_items');
+    }
+}
